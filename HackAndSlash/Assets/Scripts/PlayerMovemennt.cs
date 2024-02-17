c09ce1ad7b22de3b3086c4615d80fc743285a605
SuryaKumar135 <122425952+SuryaKumar135@users.noreply.github.com>
2024-02-17 12:54:27 +0530

    just

--- HackAndSlash/Assets/Scripts/PlayerMovemennt.cs
+++ HackAndSlash/Assets/Scripts/PlayerMovemennt.cs
@@ -14,82 +14,73 @@ public class PlayerMovemennt : MonoBehaviour
 
 
     public float value = 0;
-    
+
     HackAndSlash inputs;
 
-    
+    private CharacterController characterController;
+
+
     int Speed = 5;
     private void Awake()
     {
-        equip=GetComponent<SwordEquip>();
+        equip = GetComponent<SwordEquip>();
         animations = GetComponent<PlayerAnimations>();
+        characterController = GetComponent<CharacterController>();
         inputs = new HackAndSlash();
         inputs.Player.Run.performed += Sprinting;
         inputs.Player.Run.canceled += ResetMethod;
-        inputs.Player.SwordEquip.performed+= equip.SwordEquipAndUnEquip;
+        inputs.Player.SwordEquip.performed += equip.SwordEquipAndUnEquip;
         //inputs.Player.Fire.performed += Slash;
         //inputs.Player.Fire.canceled -= Slash; 
+        
     }
     public Vector2 moveinput;
     public Vector3 movinginput;
+    public float runSpeed;
+    public float walkSpeed;
+    private float playerSpeed;
+    public Vector3 direction;
     public float animeValue, walkAnimeValue = 0.5f, runAnimeValue = 1f;
-   
+
     private void Update()
     {
         Movement();
-        
+
     }
     public void Slash(InputAction.CallbackContext obj)
     {
         Debug.Log("attacked");
         //animations.SetTrigger("IsAttacked");
-        
+
     }
+    public float y;
     public void Movement()
     {
         moveinput = inputs.Player.Move.ReadValue<Vector2>();
         movinginput.x = moveinput.x;
         movinginput.z = moveinput.y;
-        
+        direction = new Vector3(moveinput.x * playerSpeed, y, moveinput.y * playerSpeed);
 
 
-        if(inputs.Player.Run.IsPressed())
+        characterController.Move(direction * Time.deltaTime);
+
+
+        if (inputs.Player.Run.IsPressed())
         {
             animeValue = runAnimeValue;
+            playerSpeed = runSpeed;
 
-           
+
         }
         else
         {
             animeValue = walkAnimeValue;
+            playerSpeed = walkSpeed;
         }
-        
-        animations.MovementAnimation(moveinput.x*animeValue,moveinput.y*animeValue);
 
-        //if (moveinput.x != 0 || moveinput.y != 0)
-        //{
-        //    if (value >= -1)
-        //    {
-        //        value += -1 * Time.deltaTime;
-        //    }
-        //    animations.SetFloat("Xvalue", value);
-        //    animations.SetFloat("Yvalue", 0);
-        //    transform.Translate(movinginput * Speed * Time.deltaTime);
-        //    animations.SetBool("isWalking", true);
+        animations.MovementAnimation(moveinput.x * animeValue, moveinput.y * animeValue);
 
-         
-        //}
-    //    else
-    //    {
-    //        animations.SetBool("isWalking", false);
 
-    //        if (value >= 0)
-    //        {
-    //           value += 1 * Time.deltaTime;
-    //        }
-    //    }
-
-    //    Debug.Log(moveinput);
     }
 
     private void OnEnable()
@@ -100,12 +91,12 @@ public class PlayerMovemennt : MonoBehaviour
 
     private void OnDisable()
     {
-        inputs.Disable();   
+        inputs.Disable();
     }
     public void Sprinting(InputAction.CallbackContext obj)
-    { 
-      
-        animeValue=runAnimeValue;
+    {
+
+        animeValue = runAnimeValue;
         Debug.Log("running");
 
     }
@@ -116,7 +107,6 @@ public class PlayerMovemennt : MonoBehaviour
         animeValue = walkAnimeValue;
         //Debug.Log("not running");
     }
-
 
 
 }
